6453157ce967405747d2fdfde2e33a10b5e86a9f
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-21 17:36:37 +0200

    Install mac app store apps and update those

---
+++ MacAppStore/Upgrade.sh
@@ -0,0 +1,4 @@
+mas upgrade 443987910 #1Password
+mas upgrade 409201541 #Pages
+mas upgrade 409183694 #Keynote
+mas upgrade 409203825 #Numbers
--- installation.sh
+++ installation.sh
@@ -6,11 +6,17 @@ $DIR/Wizards/MacSettings.sh
 notify "Installing Brew"
 $DIR/Wizards/Brew.sh
 
+notify "Upgrade brew"
+brew update && brew upgrade
+
 notify "Installing Brew Bundles"
 brew bundle --file=Brew/Brewfile
 
 notify "Installing Brew Cask Bundles"
 brew bundle --file=Brew/Caskfile
+
+notify "Installing and/or upgrading mac app store apps"
+$DIR/MacAppStore/Upgrade.sh
 
 notify "Installing OhMyZsh"
 $DIR/Wizards/OhMyZsh.sh
