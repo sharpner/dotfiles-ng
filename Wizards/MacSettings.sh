a3a1ebf2936c8e8b77db19c61c0422e62f88a6ee
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-21 16:06:58 +0200

    Cask and mac settings

---
+++ Wizards/MacSettings.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source 'base.sh'
+
+#setup machines hostname
+echo "Input hostname for this machine"
+read hostname
+
+if [ -z "$hostname" ] ; then
+  echo "Hostname must be set."
+  exit 1
+fi
+
+sudo scutil --set ComputerName $hostname
+sudo scutil --set HostName $hostname
+sudo scutil --set LocalHostName $hostname
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $hostname
+
+# disable mouse aceleration
+sudo defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+# enable full keyboard access aka "cancel on space"
+sudo defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# don't automatically open photos
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
--- installation.sh
+++ installation.sh
@@ -1,10 +1,16 @@
 source 'base.sh'
 
+notify "Configuring your mac"
+$DIR/Wizards/MacSettings.sh
+
 notify "Installing Brew"
 $DIR/Wizards/Brew.sh
 
 notify "Installing Brew Bundles"
 brew bundle --file=Brew/Brewfile
+
+notify "Installing Brew Cask Bundles"
+brew bundle --file=Brew/Caskfile
 
 notify "Installing OhMyZsh"
 $DIR/Wizards/OhMyZsh.sh
