4d51f242b114b618ec1253796d77220266214613
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-21 16:22:15 +0200

    More default settings for mac

--- Wizards/MacSettings.sh
+++ Wizards/MacSettings.sh
@@ -29,4 +29,25 @@ sudo defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # don't automatically open photos
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
+#disable default save to cloud
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+#disable app quarantine
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable smart quotes and dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Remove useless icons from Safari’s bookmarks bar
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+# Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+#disable startup sound
+sudo nvram SystemAudioVolume=%80
+
 touch $LOCK_DIR/$LOCK
