6da2400114a55ce77b0612dee8f222c5f3d16f65
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-21 21:54:41 +0200

    Add missing app store apps

--- MacAppStore/Upgrade.sh
+++ MacAppStore/Upgrade.sh
@@ -2,3 +2,7 @@ mas upgrade 443987910 #1Password
 mas upgrade 409201541 #Pages
 mas upgrade 409183694 #Keynote
 mas upgrade 409203825 #Numbers
+mas upgrade 824171161 #Affinity Designer
+mas upgrade 497799835 #Xcode
+mas upgrade 824183456 #Affinity Photo
+mas upgrade 410628904 #Wunderlist
