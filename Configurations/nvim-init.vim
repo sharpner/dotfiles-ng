eb4281c94b637ea7d7f78299e205b6fe7df33db3
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-21 16:48:07 +0200

    Start of new neovim setup

---
+++ Configurations/nvim-init.vim
@@ -0,0 +1,8 @@
+" plugin setup
+call plug#begin('~/.vim/plugged')
+
+" vim-go is just awesome
+Plug 'fatih/vim-go', { 'tag': '*' }
+
+call plug#end()
+
--- Wizards/NeoVim.sh
+++ Wizards/NeoVim.sh
@@ -3,7 +3,10 @@ source 'base.sh'
 
 function setup {
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
+    mkdir -p ~/.config/nvim/
+    ln -sF $CONFIG_DIR/nvim-init.vim ~/.config/nvim/init.vim 
+    nvim +PlugUpdate
     return $?
 }
 
