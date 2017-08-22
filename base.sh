296db9308b04e3b3383ff52b4ba9156c722100db
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-22 19:56:13 +0200

    Prevent installation without settings file

--- base.sh
+++ base.sh
@@ -2,9 +2,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/settings.dist.sh
-if [ -f $DIR/settings.sh ] ; then
-	source $DIR/settings.sh
+if [ ! -f $DIR/settings.sh ] ; then
+    echo "Please provide a settings.sh file (@see settings.dist.sh)"
+    exit 1
 fi
+
+source $DIR/settings.sh
 
 function notify {
     echo "\033[0;32m####### $1 ########\033[0m"
