d767f5f55145c20ed8f6ddbed7cee98ce5073fa4
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-23 13:52:06 +0200

    Alacritty base setup

---
+++ Wizards/Alacitty.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source 'base.sh'
+
+function setup {
+    FOLDER=`mktemp -d`
+    export PATH=$PATH:$HOME/.cargo/bin
+    curl https://sh.rustup.rs -ysSf | sh && \
+    git clone https://github.com/jwilm/alacritty.git $FOLDER && \
+    rustup override set stable && \
+    rustup update stable && \
+    cd $FOLDER && \
+    cargo build --release && \
+    make app && \
+    cp -r target/release/osx/Alacritty.app /Applications/Alacritty.app && \
+    curl -L https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.otf -o ~/Library/Fonts/Fura Mono Regular Nerd Font Complete Mono.otf
+    return $?
+}
+
+LOCK=alacritty.lock
+[ -f $LOCK_DIR/$LOCK ] || setup && touch $LOCK_DIR/$LOCK
