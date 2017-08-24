28b5908e510a770b12064a3f04c06cae3625018b
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-24 22:23:11 +0200

    Improved git configurations
    gpg and global ignores

--- Wizards/GitSetup.sh
+++ Wizards/GitSetup.sh
@@ -8,3 +8,8 @@ git config --global user.signingkey $GIT_GPG_ID
 git config --global commit.gpgsign true
 git config --global gpg.program gpg
 git config --global core.editor nvim
+git config --global core.excludesfile $CONFIG_DIR/gitignore_global
+
+gpg --allow-secret-key-import --import $GPG_KEY
+mkdir -p ~/.gnupg
+cp $CONFIG_DIR/gpg-agent.conf ~/.gnupg/
--- settings.dist.sh
+++ settings.dist.sh
@@ -1,3 +1,4 @@
 GIT_USERNAME=johnhancock
 GIT_EMAIL=johnhancock@example.com
 GIT_GPG_ID=deadbeef
+GPG_KEY=~/gpg.asc
