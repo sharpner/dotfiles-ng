3c29d636d620ed62d40c5d960070bc539fd2b326
n.wagensonner <n.wagensonner@manyminds.de>
2017-08-24 21:37:11 +0200

    Correctly link alacritty config

--- base.sh
+++ base.sh
@@ -13,9 +13,6 @@ function notify {
     echo "\033[0;32m####### $1 ########\033[0m"
 }
 
-TARGET_CONFIG_DIR=~/.dotfiles-config
 CONFIG_DIR=$DIR/Configurations
 SCRIPTS_DIR=$DIR/Scripts
 LOCK_DIR=$DIR/Locks
-
-[ -d $TARGET_CONFIG_DIR ] || mkdir $TARGET_CONFIG_DIR
