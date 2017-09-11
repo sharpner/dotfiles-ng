143f6da72cd3bc1d0a310c9379ee6cd99fd371ef
n.wagensonner <n.wagensonner@manyminds.de>
2017-09-11 10:49:09 +0200

    Source env vars for setup

--- Wizards/Alacritty.sh
+++ Wizards/Alacritty.sh
@@ -4,7 +4,7 @@ source 'base.sh'
 function setup {
     FOLDER=`mktemp -d`
     export PATH=$PATH:$HOME/.cargo/bin
-    curl https://sh.rustup.rs -ysSf | sh && \
+    curl https://sh.rustup.rs -sSf | sh -s -- -y && \
     git clone https://github.com/jwilm/alacritty.git $FOLDER && \
     rustup override set stable && \
     rustup update stable && \
@@ -12,7 +12,7 @@ function setup {
     cargo build --release && \
     make app && \
     cp -r target/release/osx/Alacritty.app /Applications/Alacritty.app && \
-    mkdir -p ~/.config/alacritty
+    mkdir -p ~/.config/alacritty && \
     ln -sF $CONFIG_DIR/alacritty.yml ~/.config/alacritty &&
     curl -L https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.otf\?raw=true -o ~/Library/Fonts/Fura\ Mono\ Regular\ Nerd\ Font\ Complete\ Mono.otf
     return $?
@@ -20,3 +20,4 @@ function setup {
 
 LOCK=alacritty.lock
 [ -f $LOCK_DIR/$LOCK ] || setup && touch $LOCK_DIR/$LOCK
+
