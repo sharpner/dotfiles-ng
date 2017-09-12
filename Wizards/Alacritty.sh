f24b26a78981af6a6b49e2c30a56ba6a43255032
Nino <n.wagensonner@manyminds.de>
2017-09-12 15:18:44 +0200

    Alacritty fixes (#5)
    * Source env vars for setup
    
    * Correctly link configuration
    
    * Fix symlink for alacritty

--- Wizards/Alacritty.sh
+++ Wizards/Alacritty.sh
@@ -4,7 +4,7 @@ source 'base.sh'
 function setup {
     FOLDER=`mktemp -d`
     export PATH=$PATH:$HOME/.cargo/bin
-    curl https://sh.rustup.rs -ysSf | sh && \
+    curl https://sh.rustup.rs -sSf | sh -s -- -y && \
     git clone https://github.com/jwilm/alacritty.git $FOLDER && \
     rustup override set stable && \
     rustup update stable && \
@@ -12,8 +12,8 @@ function setup {
     cargo build --release && \
     make app && \
     cp -r target/release/osx/Alacritty.app /Applications/Alacritty.app && \
-    mkdir -p ~/.config/alacritty
-    ln -sF $CONFIG_DIR/alacritty.yml ~/.config/alacritty &&
+    mkdir -p ~/.config/alacritty && \
+    ln -F $CONFIG_DIR/alacritty.yml ~/.config/alacritty/alacritty.yml &&
     curl -L https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.otf\?raw=true -o ~/Library/Fonts/Fura\ Mono\ Regular\ Nerd\ Font\ Complete\ Mono.otf
     return $?
 }
