9b231d72d9add023830d9f2f9383899a70c3a094
n.wagensonner <n.wagensonner@manyminds.de>
2017-09-14 20:00:10 +0200

    Improved icon for alacritty
    Also installs in home instead of global now

--- Wizards/Alacritty.sh
+++ Wizards/Alacritty.sh
@@ -9,9 +9,13 @@ function setup {
     rustup override set stable && \
     rustup update stable && \
     cd $FOLDER && \
+    # patch icon :)
+    wget https://github.com/jwilm/alacritty/files/1155751/icons.zip && \
+    unzip icons.zip && \
+    cp $FOLDER/full_blue.icns $FOLDER/assets/osx/Alacritty.app/Contents/Resources/alacritty.icns && \
     cargo build --release && \
     make app && \
-    cp -r target/release/osx/Alacritty.app /Applications/Alacritty.app && \
+    cp -r target/release/osx/Alacritty.app ~/Applications/Alacritty.app && \
     mkdir -p ~/.config/alacritty && \
     ln -F $CONFIG_DIR/alacritty.yml ~/.config/alacritty/alacritty.yml &&
     curl -L https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.otf\?raw=true -o ~/Library/Fonts/Fura\ Mono\ Regular\ Nerd\ Font\ Complete\ Mono.otf
