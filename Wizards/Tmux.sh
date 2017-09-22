caa1acbea790da21e15247fafa4ba8fbad5df6f8
n.wagensonner <n.wagensonner@manyminds.de>
2017-09-22 15:05:11 +0200

    Install tpm for tmux and activate a few plugins

--- Wizards/Tmux.sh
+++ Wizards/Tmux.sh
@@ -2,7 +2,14 @@
 source 'base.sh'
 
 function setup {
-    ln -sF $CONFIG_DIR/tmux.conf ~/.tmux.conf
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
+    ln -sF $CONFIG_DIR/tmux.conf ~/.tmux.conf && \
+    # start a server but don't attach to it
+    tmux start-server && \
+    # create a new session but don't attach to it either
+    tmux new-session -d && \
+    # install the plugins
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
     return $?
 }
 
