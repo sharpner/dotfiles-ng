b46ed9b9cf1c6cfae4fe504a30ac482e998adab3
n.wagensonner <n.wagensonner@manyminds.de>
2018-02-03 11:06:42 +0100

    Enable diff so fancy by default

--- Wizards/GitSetup.sh
+++ Wizards/GitSetup.sh
@@ -13,3 +13,22 @@ git config --global core.excludesfile $CONFIG_DIR/gitignore_global
 gpg --allow-secret-key-import --import "$GPG_KEY"
 mkdir -p ~/.gnupg
 cp $CONFIG_DIR/gpg-agent.conf ~/.gnupg/
+
+# diff so fancy settings
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+git config --bool --global diff-so-fancy.markEmptyLines false
+
+
