9db2a60574a13252ecd5297ddae0e504e2dcfd6e
n.wagensonner <n.wagensonner@manyminds.de>
2018-02-03 10:50:56 +0100

    Return to default keyresponses

--- Wizards/MacSettings.sh
+++ Wizards/MacSettings.sh
@@ -51,7 +51,7 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 sudo nvram SystemAudioVolume=%80
 
 # keyboard delay
-defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 
 touch $LOCK_DIR/$LOCK
