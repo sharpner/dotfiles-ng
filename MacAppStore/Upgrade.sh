b55331351c32936bb96464795582ceb97337ac8f
Nino Wagensonner <n.wagensonner@manyminds.de>
2018-04-16 09:19:57 +0200

    Android studio env vars
    Also removes telegram

--- MacAppStore/Upgrade.sh
+++ MacAppStore/Upgrade.sh
@@ -1,9 +1,8 @@
-mas upgrade 409183694 #Keynote
-mas upgrade 409201541 #Pages
-mas upgrade 409203825 #Numbers
-mas upgrade 410628904 #Wunderlist
-mas upgrade 443987910 #1Password
-mas upgrade 497799835 #Xcode
-mas upgrade 747648890 #Telegram
-mas upgrade 824171161 #Affinity Designer
-mas upgrade 824183456 #Affinity Photo
+mas install 409183694 #Keynote
+mas install 409201541 #Pages
+mas install 409203825 #Numbers
+mas install 410628904 #Wunderlist
+mas install 443987910 #1Password
+mas install 497799835 #Xcode
+mas install 824171161 #Affinity Designer
+mas install 824183456 #Affinity Photo
