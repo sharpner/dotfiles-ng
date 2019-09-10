110429f077906c40ecf72fff5617b8a424c140bb
n.wagensonner <n.wagensonner@manyminds.de>
2019-09-10 21:32:59 +0200

    Use stable alacritty

--- MacAppStore/Upgrade.sh
+++ MacAppStore/Upgrade.sh
@@ -3,6 +3,3 @@ mas install 409201541 #Pages
 mas install 409203825 #Numbers
 mas install 410628904 #Wunderlist
 mas install 443987910 #1Password
-mas install 497799835 #Xcode
-mas install 824171161 #Affinity Designer
-mas install 824183456 #Affinity Photo
--- Wizards/Alacritty.sh
+++ Wizards/Alacritty.sh
@@ -11,24 +11,6 @@ if [ "$KEYBOARD_LAYOUT" == "de" ]; then
 fi
 
 function setup {
-    FOLDER=`mktemp -d`
-    export PATH=$PATH:$HOME/.cargo/bin
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
-    git clone https://github.com/jwilm/alacritty.git $FOLDER
-    git checkout v0.2.0
-    rustup override set stable
-    rustup update stable
-    cd $FOLDER
-    # patch mac os x fallback font
-    curl https://github.com/algesten/alacritty/commit/1ecd26ef0467b367d8c59522ca0c60004f7a6436.patch | git apply
-    # patch icon :)
-    wget https://github.com/jwilm/alacritty/files/1155751/icons.zip
-    unzip icons.zip && \
-    cp $FOLDER/original.icns $FOLDER/assets/osx/Alacritty.app/Contents/Resources/alacritty.icns
-    cargo build --release
-    make app
-    $(! test -d ~/Applications/) && mkdir ~/Applications
-    cp -r target/release/osx/Alacritty.app ~/Applications/Alacritty.app
     mkdir -p ~/.config/alacritty
     ln -F $CONFIG_DIR/$FILENAME ~/.config/alacritty/alacritty.yml
     curl -L https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.otf\?raw=true -o ~/Library/Fonts/Fura\ Mono\ Regular\ Nerd\ Font\ Complete\ Mono.otf
